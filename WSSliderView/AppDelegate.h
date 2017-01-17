34f76df8262d81ca802d22674538b80e182afbc0
Scott Wathen <swathen@hotmail.com>
2017-01-16 19:05:07 -0500

    Initial Commit

---
+++ WSSliderView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSSliderView
+//
+//  Created by Scott Wathen on 1/16/17.
+//  Copyright © 2017 wogsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSSliderView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSSliderView
+//
+//  Created by Scott Wathen on 1/16/17.
+//  Copyright © 2017 wogsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
